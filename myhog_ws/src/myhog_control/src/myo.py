cb4c15709aada9516b2f79f5a04efa411997ef90
MikhailTodes <mikhailtodes2016@u.northwestern.edu>
2016-03-14 09:56:10 -0500

    Added shutdown commands and moved opening and closing serial

--- myhog_ws/src/myhog_control/src/myo.py
+++ myhog_ws/src/myhog_control/src/myo.py
@@ -12,34 +12,18 @@ yaw = 3750
 pitch = 3750
 
 def xbeeSend(event):
-    xbee = serial.Serial(
-        port='/dev/ttyUSB0', 
-        baudrate=57600, 
-        timeout=1,
-        parity=serial.PARITY_NONE,
-        stopbits=serial.STOPBITS_ONE,
-        bytesize=serial.EIGHTBITS,
-        rtscts=False,
-        dsrdtr=False
-    )
-
+   
     xbee.flushInput()
     xbee.flushOutput()
-    rospy.sleep(0.2)    
+    
     global gest
     global yaw
     global pitch
     
     msg = "{:d},{:d},{:d}\n".format(gest,pitch,yaw)
-    print msg
     xbee.write(msg)
-
-    rospy.sleep(0.2)
-    xbee.flushInput()
-    xbee.flushOutput()
-    
+    print msg
     #rospy.loginfo(rospy.get_caller_id() + "I heard %s", msg)
-    xbee.close()
 
 
 def gestCallback(data):
@@ -51,8 +35,6 @@ def gestCallback(data):
     else:
         gest = 0
 
-
-
 def imuCallback(data):
 
     global yaw
@@ -60,22 +42,44 @@ def imuCallback(data):
 
     (y,p,r) = euler_from_quaternion([data.orientation.x,data.orientation.y,data.orientation.z, data.orientation.w])
 
-    y = (((y*57.3065)+90)*(25))+1500
+    y = (y*57.3065)
+    if (y>0): #positive y
+        y = (((-180+y)+90)*(25))+1500
+    else: #negative y
+        y = (((180+y)+90)*(25))+1500
     p = (((p*57.3065)+90)*(25))+1500
 
     yaw = int(y)
     pitch = int(p)
   
     
+def closingStatements():
+    xbee.close()
+    print "Thank you for playing!"
+
+
 if __name__ == '__main__':
 
+    xbee = serial.Serial(
+        port='/dev/ttyUSB0', 
+        baudrate=57600, 
+        timeout=1,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.EIGHTBITS,
+        rtscts=False,
+        dsrdtr=False
+    )
 
     rospy.init_node('serial_send', anonymous=True)
 
-    rospy.Timer(rospy.Duration(0.0001), xbeeSend)#Every half second - edit later to push for speed
+    rospy.Timer(rospy.Duration(0.1), xbeeSend)#Every tenth of a second - edit later to push for speed
     
     rospy.Subscriber("/myo_gest", UInt8, gestCallback)
     rospy.Subscriber("/myo_imu", Imu, imuCallback)
     
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
+
+    rospy.on_shutdown(closingStatements)
+    
